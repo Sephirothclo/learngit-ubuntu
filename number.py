79ec5ba6be05227207f7748ec1d9ae8d537cc442
cloud <3553605748@qq.com>
2024-05-16 17:30:38 +0800

    add two file

---
+++ number.py
@@ -0,0 +1,10 @@
+number=14_000_000
+print(number)
+print('*'*40)
+print(5+3)
+a=5
+b=3
+print(a+b)
+c=a+b
+print(c)
+import this
---
+++ string.py
@@ -0,0 +1,23 @@
+name="sephiroth and cloud"
+print(name.title())
+print(name.upper())
+print(name.lower())
+print('*'*40)
+name_first='sephiroth'
+name_last='cloud'
+name=f'{name_first} {name_last}'
+print(name.title())
+print('*'*40)
+n='python '
+print(n)
+print(n.rstrip())
+print(n)
+m=n.rstrip()
+print(m)
+a=' akaashi '
+print(a.rstrip())
+print(a.lstrip())
+print(a.strip())
+print('*'*40)
+a='ad ms "msby"'
+print(a)
